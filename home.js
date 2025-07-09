96d0ae5a0c1884b89f40c5ed1ef70db4ace1fede
Sindhu <sindhujaranikancharla9@gmail.com>
2025-07-09 09:38:55 +0530

    home.html

---
+++ home.js
@@ -0,0 +1,29 @@
+//Reference data types
+//Objects
+//Arrays
+//Functions 
+
+//variable names should contain $,_ or alphabet
+//variable names should not start with numbers
+//kebab case
+var user = {
+    'firstname': "sindhu",
+    age: 25,
+    Profession:"Software Engineer",
+    isMarried: true,
+    address: {
+        city:"Chennai",
+        State:"Tamilnadu",
+        Country:"India",
+        pincode: {
+            code:"12345",
+        },
+
+    },
+
+
+};
+console.log(user);
+delete user.isMarried;
+
+
